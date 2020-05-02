9ec29345e02f8b7e702dcb93bce0343510c83d23
sharanr91 <sharanr91@Dear.General.github.com>
2020-05-02 13:45:29 +0800

    a bit of typescript with RxJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,17 @@ export class AppComponent {
   constructor(private wikipedia: WikipediaService) { }
 
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+
+    /*
+    this.wikipedia.search(term) -> retrieves an observable
+    and that observable is gonna start to emit events as soon as it retrieves some data
+    from the wikipedia api.
+    .subscribe((response: any) => {...} )-> we call subscribe on it and
+    pass the observer -> (response: any) => {this.pages = response.query.search;} -> this function
+    is going to be called whenever our observable emits some value.
+    */
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
     })
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse{
+  query:{
+    search:{
+      title:string;
+      snippet:string;
+      pageid:number;
+    }[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +20,10 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   public search(term: string) {
-    return this.http.get('https://en.wikipedia.org/w/api.php', {
+    // http.get returns an observable
+    // as soon as the request is completed, 
+    //the observable is going to emit an value consisting of all the data that is retreived from that api
+    return this.http.get<WikipediaResponse>('https://en.wikipedia.org/w/api.php', {
       params: {
         action: "query",
         format: "json",
@@ -18,6 +32,9 @@ export class WikipediaService {
         srsearch: term,
         origin: '*'
       }
-    });
-  }
+    }).pipe(
+      pluck('query', 'search')
+    )
+  };
+
 }
